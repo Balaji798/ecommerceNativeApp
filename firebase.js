e41b23e12549489fe4aa895a4e3b0f0158b58157
MetaUnite <Support@metaunite.in>
2023-02-07 10:27:50 +0530

    new update

--- firebase.js
+++ firebase.js
@@ -1,8 +1,10 @@
+import {STORAGEBUCKET,MESSAGINGSENDERID,APPID,PROJECTID,APIKEY} from "@env"
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyDmB6nn6KSQOhtjkw3PNVmggP4O70hvWkU",
-  authDomain: "eshop-980cd.firebaseapp.com",
-  projectId: "eshop-980cd",
-  storageBucket: "eshop-980cd.appspot.com",
-  messagingSenderId: "228076181612",
-  appId: "1:228076181612:web:cbe88bf0873ebd22ed2049",
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket:STORAGEBUCKET,
+  messagingSenderId:MESSAGINGSENDERID,
+  appId: APPID,
 };
